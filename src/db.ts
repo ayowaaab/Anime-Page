104ee38bf8fe8a15a70bfbbcfb39b95dc2579004
ayowaaab <ayoub.dahmen@esprit.tn>
2024-01-22 08:54:20 +0100

    new DB modifications

--- src/db.ts
+++ src/db.ts
@@ -1,29 +1,31 @@
 export const items = [
-    {
-      id: 1,
-      heading:"Jujutsu Kaisen" ,
-      description: `"The right death" is what a boy fights for in the midst of the curses that plague our daily lives. 
+  {
+    id: 1,
+    heading: "Jujutsu Kaisen",
+    description: `"The right death" is what a boy fights for in the midst of the curses that plague our daily lives. 
     These curses are born from negative emotions like hardship, regret, and shame, 
     and they can wreak havoc on people's lives, often resulting in death. Ironically, 
     the only way to banish these curses is by invoking another curse. Itadori Yuji is an average high 
     schooler with extraordinary physical abilities. But when he consumes the finger`,
-      img: "caroselPics/carouselGojo.jpg",
-    },
-    {
-      id: 2,
-      heading:"Eminence in shadow" ,
-      description: `"The right death" is what a boy fights for in the midst of the curses that plague our daily lives. 
-    These curses are born from negative emotions like hardship, regret, and shame, 
-    and they can wreak havoc on people's lives, often resulting in death. Ironically, 
-    the only way to banish these curses is by invoking another curse. Itadori Yuji is an average high 
-    schooler with extraordinary physical abilities. But when he consumes the finger`,
-      img: "caroselPics/shadow.png",
-    },
-    {
-      id: 3,
-      heading:"Solo Leveling" ,
-      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quibusdam mollitia,
-        pariatur quam, quod omnis dolore facere eveniet libero nulla quo ut, distinctio fugiat iure."`,
-      img: "caroselPics/soloLeveling.jpg",
-    },
-  ];
+    img: "caroselPics/carouselGojo.jpg",
+  },
+  {
+    id: 2,
+    heading: "Eminence in shadow",
+    description: `this story is of a young man named Cid Kageno, 
+    who has always dreamed of living a life of power and influence, but from behind the scenes. 
+    When he's unexpectedly reincarnated into a fantasy world, he takes on the identity of "Shadow" 
+    and sets out to create a secret organization that will allow him to achieve his goals."`,
+    img: "caroselPics/soloLeveling.jpg",
+
+  },
+  {
+    id: 3,
+    heading: "Solo Leveling",
+    description: `The story follows Jinwoo as he embarks on various missions and battles,
+    encountering a variety of monsters and challenges along the way. As he becomes more powerful, 
+    he also learns more about the mysterious world of hunters and the dangers they face."`,
+
+    img: "caroselPics/soloLeveling.jpg",
+  },
+];
